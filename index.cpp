19a5b98ff393047167b7b25831cf4b7738515b50
Oscar <loxn52@gmail.com>
2018-05-31 00:16:02 -0500

    El archivo main.cpp es donde se empezara

---
+++ index.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+class nodo_consulta{
+
+};
+
+class nodo{
+
+};
+
+class arbol{
+
+};
+
+int main(){
+
+  return 0;
+}
