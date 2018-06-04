0c4a22a3ab727d510dfa3f2fde714379c0518b8a
Oscar <loxn52@gmail.com>
2018-06-04 12:04:20 -0500

    Leer archivo

--- index.cpp
+++ index.cpp
@@ -16,6 +16,30 @@ class arbol{
 };
 
 int main(){
+  FILE * reglas;
+  char **cadena, *aux_cadena;
+  int cont_saltos=1, c;
+  reglas=fopen("reglas.csv", "r+");
+  if(reglas==NULL){
+    printf("\nNo se pudo abrir archivo");
+  }else{
+    //aqui se empieza a construir arbol
+    do{
+      c=fgetc(reglas);
+      if(c==10){
+        cont_saltos++;
+      }
+    }while(c!=EOF);
+    cadena=(char**)calloc(cont_saltos, sizeof(char*));
+    printf("\nTermina do while y crea arreglo");
+    rewind(reglas);
+    for(int i=0;i=3;i++){
+      fgets(aux_cadena, 100, reglas);
+      printf("%s", aux_cadena);
+      cadena[i]=aux_cadena;
+    }
 
+    fclose(reglas);
+  }
   return 0;
 }
